258bd3ff9f659377095d741e11026c51141fc362
kargo <karenrgo@gmail.com>
2019-08-28 14:46:35 -0700

    added provider action and store

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux'
+import store from './store'
 
 function App() {
   return (
-    <div className="App">
+    <Provider store={store}>
       <header className="App-header">
         <h1>capsule curate</h1>
       </header>
-    </div>
+    </Provider>
   );
 }
 
