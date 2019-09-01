abd740a76310e7d3b823cefa896a10fbc3e7f59a
kargo <karenrgo@gmail.com>
2019-08-31 20:30:34 -0700

    trying flexbox for the capsule item show view... it is ok.

--- src/containers/CapsuleContainer.js
+++ src/containers/CapsuleContainer.js
@@ -11,7 +11,7 @@ const CapsuleContainer = ({ show_capsule }) => {
 
   if (show_capsule) {
     return (
-      <div id='grid-parent' className='capsule container'>
+      <div id='grid-parent'>
         {/* <h2>Current Capsule: {show_capsule.title}</h2> */}
         {/* <h4>(Number of Items) {show_capsule.items.length}</h4> */}
         {show_capsule.items.map(item => <Item key={item.id} item={item} />)}
