c2c52f51eb911c0473ebcfcaff20b9cec4dced31
kargo <karenrgo@gmail.com>
2019-08-31 17:31:54 -0700

    added padding to container and about lorem ipsum

--- src/components/About.js
+++ src/components/About.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 const About = () => {
   return (
-    <React.Fragment>
+    <div className='text container'>
       <h1>About Capsule Curate</h1>
       <p>who what where when why how</p>
-    </React.Fragment>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p>Purus semper eget duis at tellus. Mi tempus imperdiet nulla malesuada pellentesque elit eget gravida. Ut porttitor leo a diam sollicitudin tempor id eu nisl. Auctor urna nunc id cursus metus aliquam eleifend mi in. Ornare aenean euismod elementum nisi quis eleifend. Ornare arcu dui vivamus arcu felis bibendum ut tristique. Tempus urna et pharetra pharetra massa. Aliquet eget sit amet tellus cras adipiscing enim. Tempor commodo ullamcorper a lacus vestibulum sed arcu non. Urna nunc id cursus metus. Ultricies leo integer malesuada nunc.</p>
+    </div>
   )
 }
 
