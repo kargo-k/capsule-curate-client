33b461da404b998a963c50a4e91575d49421650e
kargo <karenrgo@gmail.com>
2019-09-01 23:21:42 -0700

    added R for user

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
-const Account = () => {
-  return (
-    <div className='text container'>
-      <h1>Account CRUD here</h1>
-      <p>who what where when why how</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <p>Purus semper eget duis at tellus. Mi tempus imperdiet nulla malesuada pellentesque elit eget gravida. Ut porttitor leo a diam sollicitudin tempor id eu nisl. Auctor urna nunc id cursus metus aliquam eleifend mi in. Ornare aenean euismod elementum nisi quis eleifend. Ornare arcu dui vivamus arcu felis bibendum ut tristique. Tempus urna et pharetra pharetra massa. Aliquet eget sit amet tellus cras adipiscing enim. Tempor commodo ullamcorper a lacus vestibulum sed arcu non. Urna nunc id cursus metus. Ultricies leo integer malesuada nunc.</p>
-    </div>
-  )
+const mapStateToProps = state => {
+  return { user: state.user }
 }
 
-export default Account
+const Account = ({ user }) => {
+
+  if (!user) {
+    return (
+      // Redirect to root if the user is not logged in
+      <Redirect to='/' />
+    )
+  } else {
+    return (<div className='user container'>
+      <h1>{user.username}</h1>
+      <h3>{user.location}</h3>
+    </div>)
+  }
+}
+
+export default connect(mapStateToProps)(Account);
