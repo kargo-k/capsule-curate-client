572e9ff14fc754f169d3060a450d25464d2ad52c
kargo <karenrgo@gmail.com>
2019-09-06 15:26:32 -0700

    styled remove button

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { showItem } from '../actions';
+import { Link } from 'react-router-dom';
 
 const mapDispatchToProps = dispatch => {
   return { show_item: (payload) => dispatch(showItem(payload)) }
@@ -20,15 +21,22 @@ const Item = props => {
     <span className='item-details' onClick={() => handleOnClick(props.item)}>
       <img src={props.item.image} alt={props.item.name} />
 
-      <span className='slider' >
-        <div className='text'>
-          <div className='item-link' to='/item'>{props.item.name}
-            <div className='subtext'>{props.item.brand} / {props.item.price}</div>
-            <button onClick={() => props.removeItem({ capsule_id: props.capsule_id, item_id: props.item.id })}>Remove Item</button>
+      <Link className='item-link' to='/item'>
+        <span className='slider' >
+          {/* The button to remove an item from a capsule only appears when the item is viewed inside a capsule */}
+          {props.removeItem &&
+            <button className='remove-btn' onClick={() => props.removeItem({ capsule_id: props.capsule_id, item_id: props.item.id })}>Remove</button>}
+
+
+          <div className='text'>{props.item.name}
+            <div className='subtext'>{props.item.brand}</div>
           </div>
 
-        </div>
-      </span>
+
+
+
+        </span>
+      </Link>
 
 
 
