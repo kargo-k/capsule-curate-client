6a594bcbc970547d2e6b06901e7377ba24d4e08a
kargo <karenrgo@gmail.com>
2019-09-11 17:41:08 -0700

    updated url for api

--- src/constants/api-url.js
+++ src/constants/api-url.js
@@ -1,2 +1,2 @@
-export const API = 'http://localhost:8888/api/v1';
+export const API = 'https://capsule-curate-server.herokuapp.com/api/v1';
 export const WEATHER = 'https://weather-app-rails.herokuapp.com';
